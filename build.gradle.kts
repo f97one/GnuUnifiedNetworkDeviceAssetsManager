e4c8502487aa8efd71a38b23801b7384687236be
HAJIME Fukuna <f97one@gmail.com>
2020-08-16 11:18:27 +0900

    Lintの指摘に対応。

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
-import org.jetbrains.kotlin.gradle.dsl.Coroutines
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val flyway_version: String by project
-val postgresql_version: String by project
-val commons_cli_version: String by project
-val h2_version: String by project
-val hikari_version: String by project
-val sql2o_version: String by project
-val jbcrypt_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val flywayVersion: String by project
+val postgresqlVersion: String by project
+val commonsCliVersion: String by project
+val h2Version: String by project
+val hikariVersion: String by project
+val sql2oVersion: String by project
+val jbcryptVersion: String by project
 
 plugins {
     application
@@ -31,34 +28,34 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-html-builder:$ktorVersion")
-    implementation("io.ktor:ktor-thymeleaf:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common:$ktor_version")
-    implementation("io.ktor:ktor-server-sessions:$ktor_version")
-    implementation("io.ktor:ktor-auth:$ktor_version")
-    implementation("io.ktor:ktor-jackson:$ktor_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    implementation("io.ktor:ktor-thymeleaf:$ktorVersion")
+    implementation("io.ktor:ktor-server-host-common:$ktorVersion")
+    implementation("io.ktor:ktor-server-sessions:$ktorVersion")
+    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-jackson:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
 
-    implementation("org.mindrot:jbcrypt:$jbcrypt_version")
+    implementation("org.mindrot:jbcrypt:$jbcryptVersion")
 
     // data access
     //   flyway
-    implementation("org.flywaydb:flyway-core:$flyway_version")
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
     //   PostgreSQL JDBC Driver
-    implementation("org.postgresql:postgresql:$postgresql_version")
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
     //   H2 database
-    implementation("com.h2database:h2:$h2_version")
+    implementation("com.h2database:h2:$h2Version")
     //   HikariCP
-    implementation("com.zaxxer:HikariCP:$hikari_version")
+    implementation("com.zaxxer:HikariCP:$hikariVersion")
     //   Sql2o
-    implementation("org.sql2o:sql2o:$sql2o_version")
-    implementation("org.sql2o.extensions:sql2o-postgres:$sql2o_version")
+    implementation("org.sql2o:sql2o:$sql2oVersion")
+    implementation("org.sql2o.extensions:sql2o-postgres:$sql2oVersion")
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
