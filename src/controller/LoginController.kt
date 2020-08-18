aebb43398132850e188b3cef21f9897ecf147cdb
Kazutoshi Takemoto <k.takemoto@date-yakkyoku.com>
2020-08-18 08:17:26 +0000

    ログイン/ログアウトの処理を追加。

--- src/controller/LoginController.kt
+++ src/controller/LoginController.kt
@@ -7,15 +7,31 @@ import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
 import io.ktor.thymeleaf.ThymeleafContent
+import net.formula97.webapps.AppUserPrincipal
 import net.formula97.webapps.controller.form.LoginForm
 
 fun Route.loginController() {
-    route("/login") {
-        get {
+    route {
+        get("/login") {
+            val hasErr = call.parameters["error"] != null
             call.respond(ThymeleafContent("login", mapOf("loginForm" to LoginForm())))
         }
-        post {
 
+        get("/logout") {
+            call.sessions.clear<CurrentUserSession>()
+            call.respondRedirect("/")
+        }
+    }
+    authenticate("login") {
+        route("/login") {
+            post {
+                val principal = call.authenticate.principal<AppUserPrincipal>()
+                if (principal != null) {
+                    call.sessions.set(principal.createSession())
+                    // todo リダイレクト先を決める
+                    call.respondRedirect("/dashboards")
+                }
+            }
         }
     }
 }
