4141b8dbddb8a481147e79ccf7ec9ed950c4c93a
HAJIME Fukuna <f97one@gmail.com>
2020-09-24 21:35:16 +0900

    セッションにユーザーの権限を保持するよう修正。

--- src/AppUserPrincipal.kt
+++ src/AppUserPrincipal.kt
@@ -2,9 +2,10 @@ package net.formula97.webapps
 
 import net.formula97.webapps.entity.AppUser
 import io.ktor.auth.Principal
+import net.formula97.webapps.dao.AuthorityDao
 
 data class AppUserPrincipal(val currentUser: AppUser?): Principal {
     fun createSession(): CurrentUserSession {
-        return CurrentUserSession(currentUser!!.userId!!, currentUser.username, currentUser.displayName)
+        return CurrentUserSession(currentUser!!.userId!!, currentUser.username, currentUser.displayName, AuthorityDao().getAuthoritiesByUid(currentUser.userId!!))
     }
 }
--- src/CurrentUserSession.kt
+++ src/CurrentUserSession.kt
@@ -2,11 +2,13 @@ package net.formula97.webapps
 
 import net.formula97.webapps.dao.AppUserDao
 import net.formula97.webapps.entity.AppUser
+import net.formula97.webapps.entity.Authority
 
 data class CurrentUserSession(
     val userId: Int,
     val username: String,
-    val displayName: String?
+    val displayName: String?,
+    val authorities: List<Authority>
 ) {
     fun readUser(): AppUser {
         return AppUserDao().findByKey(AppUser(userId = userId)).get()
