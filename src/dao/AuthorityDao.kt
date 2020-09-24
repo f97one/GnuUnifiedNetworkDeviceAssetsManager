84996e0cb0f613f991062d81858d057b0ba627a7
HAJIME Fukuna <f97one@gmail.com>
2020-09-24 21:34:54 +0900

    ユーザーの権限を取得する処理を追加。

--- src/dao/AuthorityDao.kt
+++ src/dao/AuthorityDao.kt
@@ -4,4 +4,14 @@ import net.formula97.webapps.dao.config.DataSourceCreator
 import net.formula97.webapps.entity.Authority
 
 class AuthorityDao(): AbstractDao<Authority>(dataSource = DataSourceCreator.getDataSource()) {
+    fun getAuthoritiesByUid(userId: Int): List<Authority> {
+        val b = StringBuilder(basicSelectStatement(Authority::class.java))
+            .append(" where authority_id in (select authority_id from user_authority where user_id = :userId)")
+
+        val q = sql2o.open().createQuery(b.toString())
+        q.columnMappings = Authority().getDefaultMapper()
+        q.addParameter("userId", userId)
+
+        return q.executeAndFetch(Authority::class.java)
+    }
 }
