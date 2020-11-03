fc887b02a4722f1061b0f0fd607c9178dc81e6a4
HAJIME Fukuna <f97one@gmail.com>
2020-11-03 09:30:31 +0900

    H2 の PostgreSQL モードがサポートしていない関数を使っていたのを修正。 DBを作り直す必要アリ。

--- resources/db/migration/V3__increment-seq.sql
+++ resources/db/migration/V3__increment-seq.sql
@@ -1,4 +1,4 @@
-select setval('role_role_id_seq', 1);
-select setval('organization_org_id_seq', 1);
-select setval('authority_authority_id_seq', 1);
-select setval('app_user_user_id_seq', 1);
+alter sequence role_role_id_seq restart with 1;
+alter sequence organization_org_id_seq restart with 1;
+alter sequence authority_authority_id_seq restart with 1;
+alter sequence app_user_user_id_seq restart with 1;
